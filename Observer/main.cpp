ac6d2e4f3f98514a4cc31a80231a71a4864ea372
joshual777 <josui77@hotmail.com>
2019-09-12 20:55:12 -0600

    Update main.cpp

--- Observer/main.cpp
+++ Observer/main.cpp
@@ -3,30 +3,46 @@
 using namespace std;
 
 //OBSERVER IMPLEMENTATION WITH DIVISION
+//The one which has linked the observers
 class Subject {
     //The one who is receiving data from the observer
+    
+    //The vector will store the created observers
     vector < class Observer * > views;
     int value;
   public:
+    
+    //If there are observers they will be add into the vector
     void attach(Observer *obs) {
         views.push_back(obs);
     }
+    
+    //Add a value to operate
     void setVal(int val) {
         value = val;
         notify();
     }
+    
+    //Get the enter value
     int getVal() {
         return value;
     }
+    
+    //Notify of any change
     void notify();
 };
 
+//The Observers
 class Observer {
 
     Subject *model;
     int denom;
   public:
+    
+    //Recievin data to operate 
     Observer(Subject *mod, int div) {
+        
+        //Getting the variables to perate
         model = mod;
         denom = div;
         //Observers register themselves with the Subject messages
@@ -34,6 +50,8 @@ class Observer {
     }
     virtual void update() = 0;
   protected:
+    
+    //Getting teh data from the subject 
     Subject *getSubject() {
         return model;
     }
@@ -46,24 +64,32 @@ void Subject::notify() {
   //Update the data from the observer
     //It passes through all created observers
   for (int i = 0; i < views.size(); i++)
+      //Update teh data
     views[i]->update();
 }
 
+//Thep purpose of this class is to make an operation over the created observers
 class DivObserver: public Observer {
   public:
+    //Make the division process
     DivObserver(Subject *mod, int div): Observer(mod, div){}
+    //Will send the data
     void update() {
         //"Pull" information of interest
         int v = getSubject()->getVal(), d = getDivisor();
+        //Printing the data
         cout << v << " div " << d << " is " << v / d << '\n';
     }
 };
 
+//Class to update
 class ModObserver: public Observer {
   public:
     ModObserver(Subject *mod, int div): Observer(mod, div){}
     void update() {
+        //"Pull" information of interest
         int v = getSubject()->getVal(), d = getDivisor();
+        //Printing the data
         cout << v << " mod " << d << " is " << v % d << '\n';
     }
 };
